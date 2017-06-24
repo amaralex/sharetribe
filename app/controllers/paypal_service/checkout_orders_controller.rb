4d0e01e5656cf927306fab846def7abe6c9ae8d7
alexander.markov <alexander.markov@servicenow.com>
2017-06-24 23:14:44 +1000

    comment out community payment readiness check

--- app/controllers/paypal_service/checkout_orders_controller.rb
+++ app/controllers/paypal_service/checkout_orders_controller.rb
@@ -1,11 +1,11 @@
 class PaypalService::CheckoutOrdersController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  before_filter do
-    unless PaypalHelper.community_ready_for_payments?(@current_community.id)
-      render :nothing => true, :status => 400
-    end
-  end
+  #before_filter do
+  #  unless PaypalHelper.community_ready_for_payments?(@current_community.id)
+  #    render :nothing => true, :status => 400
+  #  end
+  #end
 
 
   def success
